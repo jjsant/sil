aefe0a268ce08ccba90b8e2ad63df02deac2288e
jjsant <jjsant16@hotmail.com>
2021-02-26 05:33:18 -0800

    READY FOR GIT

--- Sil.js
+++ Sil.js
@@ -21,10 +21,12 @@ function onClick(element) {
 
 // Displaying products info
 function product_Details(id) {
-  var x = document.getElementById(id);
+  let x = document.getElementById(id);
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
   } else {
     x.className = x.className.replace(" w3-show", "");
   }
 }
+
+// Slide Show function
