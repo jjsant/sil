f7340960802ad062e81516aea444ec48e3f25ebd
jjsant <jjsant16@hotmail.com>
2021-02-25 17:23:26 -0800

    dghbdhdh

--- Sil.js
+++ Sil.js
@@ -18,3 +18,13 @@ function onClick(element) {
   var captionText = document.getElementById("caption");
   captionText.innerHTML = element.alt;
 }
+
+// Displaying products info
+function product_Details(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
